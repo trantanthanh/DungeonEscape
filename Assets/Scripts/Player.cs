c16cd0fcb1a932db8e7364d183962b4672f810a8
Thành Trần Tấn <trantanthanh@gmail.com>
2024-04-08 02:48:56 +0700

    move left right by velocity of rigidbody2D

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] float moveSpeed = 3f;
+    private Rigidbody2D myRigidbody;
+    public Vector2 velocityDebug;
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        myRigidbody.velocity = new Vector2(horizontalInput, 0) * moveSpeed;
     }
 }
