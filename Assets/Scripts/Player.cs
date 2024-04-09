55d2bfc755713ca6ee3c99d69ac57def147d5c33
Thành Trần Tấn <trantanthanh@gmail.com>
2024-04-09 14:16:21 +0700

    optimize jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,7 +8,6 @@ public class Player : MonoBehaviour
     [SerializeField] float jumpForce = 5.0f;
     [SerializeField] float timeBetweenJump = 0.1f;
     private float timeNextJump = 0f;
-    private bool grounded = false;
     private Rigidbody2D myRigidbody;
     private BoxCollider2D boxCollider2D;
 
@@ -30,31 +29,32 @@ public class Player : MonoBehaviour
 
     private void CheckJump()
     {
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > timeNextJump && IsGrounded())
+        {
+            timeNextJump = Time.time + timeBetweenJump;
+            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpForce);
+        }
+    }
+
+    bool IsGrounded()
+    {
         float heightOfPlayer = boxCollider2D.size.y / 2 + 0.1f;
         RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down, heightOfPlayer, groundLayer);
-        Debug.DrawRay(transform.position, Vector2.down * heightOfPlayer, Color.green);
+        //Debug.DrawRay(transform.position, Vector2.down * heightOfPlayer, Color.green);
 
         if (hitInfo.collider != null)
         {
-            //Debug.Log($"hit : {hitInfo.collider.name}");
-            grounded = true;
+            return true;
         }
         else
         {
-            grounded = false;
-        }
-
-        if (grounded && Input.GetKeyDown(KeyCode.Space) && Time.time > timeNextJump)
-        {
-            timeNextJump = Time.time + timeBetweenJump;
-            myRigidbody.velocity += new Vector2(0, jumpForce);
+            return false;
         }
     }
 
     private void Movement()
     {
         float horizontalInput = Input.GetAxisRaw("Horizontal");
-
         myRigidbody.velocity = new Vector2(horizontalInput * moveSpeed, myRigidbody.velocity.y);
     }
 }
