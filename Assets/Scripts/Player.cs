690ddae2475b2d5bd45f6714b22f264f58b9007a
Thành Trần Tấn <trantanthanh@gmail.com>
2024-04-09 13:57:18 +0700

    add timer between jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,8 @@ public class Player : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 3f;
     [SerializeField] float jumpForce = 5.0f;
+    [SerializeField] float timeBetweenJump = 0.1f;
+    private float timeNextJump = 0f;
     private bool grounded = false;
     private Rigidbody2D myRigidbody;
     private BoxCollider2D boxCollider2D;
@@ -23,6 +25,11 @@ public class Player : MonoBehaviour
     void Update()
     {
         Movement();
+        CheckJump();
+    }
+
+    private void CheckJump()
+    {
         float heightOfPlayer = boxCollider2D.size.y / 2 + 0.1f;
         RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down, heightOfPlayer, groundLayer);
         Debug.DrawRay(transform.position, Vector2.down * heightOfPlayer, Color.green);
@@ -32,12 +39,14 @@ public class Player : MonoBehaviour
             //Debug.Log($"hit : {hitInfo.collider.name}");
             grounded = true;
         }
-        else {
+        else
+        {
             grounded = false;
         }
 
-        if (grounded && Input.GetKeyDown(KeyCode.Space))
+        if (grounded && Input.GetKeyDown(KeyCode.Space) && Time.time > timeNextJump)
         {
+            timeNextJump = Time.time + timeBetweenJump;
             myRigidbody.velocity += new Vector2(0, jumpForce);
         }
     }
