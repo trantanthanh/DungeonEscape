7ef581aaac450135c084d888350856b571b1a108
Thành Trần Tấn <trantanthanh@gmail.com>
2024-04-09 09:01:32 +0700

    update jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 3f;
+    [SerializeField] float jumpForce = 5.0f;
+    private bool grounded = false;
     private Rigidbody2D myRigidbody;
     public Vector2 velocityDebug;
     // Start is called before the first frame update
@@ -16,8 +18,25 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Movement();
+
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down);
+
+        if (hitInfo.collider != null)
+        {
+            grounded = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            myRigidbody.velocity += new Vector2(0, jumpForce);
+        }
+    }
+
+    private void Movement()
+    {
         float horizontalInput = Input.GetAxisRaw("Horizontal");
 
-        myRigidbody.velocity = new Vector2(horizontalInput, 0) * moveSpeed;
+        myRigidbody.velocity = new Vector2(horizontalInput * moveSpeed, myRigidbody.velocity.y);
     }
 }
